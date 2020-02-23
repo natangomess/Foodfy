e75a50c760bfebd701822efe1d1037bb9641cee9
Natan Gomes <55115100+natangomess@users.noreply.github.com>
2020-02-23 14:21:14 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,288 @@
+module.exports = [
+  {
+    id_recipe: 'burger.png',
+    image: '/assets/burger.png',
+    title: 'Triplo bacon burger',
+    author: 'Jorge Relato',
+    ingredients: [
+      '3 kg de carne moída (escolha uma carne magra e macia)',
+      '300 g de bacon moído',
+      '1 ovo',
+      '3 colheres (sopa) de farinha de trigo',
+      '3 colheres (sopa) de tempero caseiro: feito com alho, sal, cebola, pimenta e cheiro verde processados no liquidificador',
+      '30 ml de água gelada'
+    ],
+    preparation: [
+      'Misture todos os ingredientes muito bem e amasse para que fique tudo muito bem misturado.',
+      'Faça porções de 90 g a 100 g.',
+      'Forre um plástico molhado em uma bancada e modele os hambúrgueres utilizando um aro como base.',
+      'Faça um de cada vez e retire o aro logo em seguida.',
+      'Forre uma assadeira de metal com plástico, coloque os hambúrgueres e intercale camadas de carne e plásticos (sem apertar).',
+      'Faça no máximo 4 camadas por forma e leve para congelar.',
+      'Retire do congelador, frite ou asse e está pronto.'
+    ],
+    information: "Preaqueça a chapa, frigideira ou grelha por 10 minutos antes de levar os hambúrgueres. Adicione um pouquinho de óleo ou manteiga e não amasse os hambúrgueres! \n\n Você sabia que a receita que precede o hambúrguer surgiu no século XIII, na Europa? A ideia de moer a carne chegou em Hamburgo no século XVII, onde um açogueiro resolveu também temperá-la. Assim, a receita foi disseminada nos Estados Unidos por alemães da região. Lá surgiu a ideia de colocar o hambúrguer no meio do pão e adicionar outros ingredientes, como queijom tomates e alface."
+  },
+  {
+    id_recipe: 'pizza.png',
+    image: '/assets/pizza.png',
+    title: 'Pizza 4 estações',
+    author: 'Fabiana Melo',
+    ingredients: [
+      '1 xícara (chá) de leite',
+      '1 ovo',
+      '1 colher (chá) de sal',
+      '1 colher (chá) de açúcar',
+      '1 colher (sopa) de margarina',
+      '1 e 1/2 xícara (chá) de farinha de trigo',
+      '1 colher (sobremesa) de fermento em pó',
+      '1/2 lata de molho de tomate',
+      '250 g de mussarela ralada grossa',
+      '2 tomates fatiados',
+      'azeitona picada',
+      'orégano a gosto'
+    ],
+    preparation: [
+      'No liquidificador bata o leite, o ovo, o sal, o açúcar, a margarina, a farinha de trigo e o fermento em pó até que tudo esteja encorporado.',
+      'Despeje a massa em uma assadeira para pizza untada com margarina e leve ao forno preaquecido por 20 minutos.',
+      'Retire do forno e despeje o molho de tomate.',
+      'Cubra a massa com mussarela ralada, tomate e orégano a gosto.',
+      'Leve novamente ao forno até derreter a mussarela.'
+    ],
+    information: 'Pizza de liquidificador é uma receita deliciosa e supersimples de preparar. Feita toda no liquidificador, ela é bem prática para o dia a dia. Aqui no TudoGostoso você também encontra diversas delícias práticas feitas no liquidificador: massa de panqueca, torta de frango de liquidificador, pão de queijo de liquidificador, bolo de banana, bolo de chocolate e muito mais!'
+  },
+  {
+    id_recipe: 'espaguete.png',
+    image: '/assets/espaguete.png',
+    title: 'Espaguete ao alho',
+    author: 'Júlia Kinoto',
+    ingredients: [
+      '1 pacote de macarrão 500 g (tipo do macarrão a gosto)',
+      '1 saquinho de alho granulado',
+      '1/2 tablete de manteiga (não use margarina)',
+      '1 colher (sopa) de azeite extra virgem',
+      'ervas (manjericão, orégano, salsa, cebolinha, tomilho, a gosto)',
+      'sal',
+      '1 dente de alho',
+      'gengibre em pó a gosto',
+      '1 folha de louro'
+    ],
+    preparation: [
+      'Quando faltar mais ou menos 5 minutos para ficar no ponto de escorrer o macarrão, comece o preparo da receita.',
+      'Na frigideira quente coloque a manteiga, o azeite, a folha de louro, e o alho granulado.',
+      'Nesta hora um pouco de agilidade, pois o macarrão escorrido vai para a frigideira, sendo mexido e dosado com sal a gosto, as ervas, o gengibre em pó a gosto também.',
+      'O dente de alho, serve para você untar os pratos onde serão servidos o macarrão.',
+      'Coloque as porções nos pratos já com o cheiro do alho, e enfeite com algumas ervas.'
+    ],
+    information: 'Não lave o macarrão nem passe óleo ou gordura nele depois de escorrê-lo. Coloque direto na frigideira.'
+  },
+  {
+    id_recipe: 'lasanha.png',
+    image: '/assets/lasanha.png',
+    title: "Lasanha mac n' cheese",
+    author: 'Juliano Vieira',
+    ingredients: [
+      'massa pronta de lasanha',
+      '400 g de presunto',
+      '400 g de mussarela ralada',
+      '2 copos de requeijão',
+      '150 g de mussarela para gratinar'
+    ],
+    preparation: [
+      'Em uma panela, coloque a manteiga para derreter.',
+      'Acrescente a farinha de trigo e misture bem com auxílio de um fouet.',
+      'Adicione o leite e misture até formar um creme homogêneo.',
+      'Tempere com sal, pimenta e noz-moscada a gosto.',
+      'Desligue o fogo e acrescente o creme de leite; misture bem e reserve.'
+    ],
+    information: 'Recheie a lasanha com o que preferir.'
+  },
+  {
+    id_recipe: 'doce.png',
+    image: '/assets/doce.png',
+    title: 'Docinhos pão-do-céu',
+    author: 'Ricardo Golvea',
+    ingredients: [
+      '1 kg de batata - doce',
+      '100 g de manteiga',
+      '3 ovos',
+      '1 pacote de coco seco ralado (100 g)',
+      '3 colheres (sopa) de açúcar 1 lata de Leite Moça',
+      '1 colher (sopa) de fermento em pó',
+      'manteiga para untar',
+      'açúcar de confeiteiro'
+    ],
+    preparation: [
+      'Cozinhe a batata-doce numa panela de pressão, com meio litro de água, por cerca de 20 minutos. Descasque e passe pelo espremedor, ainda quente.',
+      'Junte a manteiga,os ovos, o coco ralado,o açúcar, o Leite Moça e o fermento em pó, mexendo bem após cada adição.',
+      'Despeje em assadeira retangular média, untada e leve ao forno médio (180°C), por aproximadamente 45 minutos. Depois de frio, polvilhe, com o',
+      'açúcar de confeiteiro e corte em quadrados.'
+    ],
+    information: ''
+  },
+  {
+    id_recipe: 'asinhas.png',
+    image: '/assets/asinhas.png',
+    title: 'Asinhas de frango ao barbecue',
+    author: 'Vania Steroski',
+    ingredients: [
+      '12 encontros de asinha de galinha, temperados a gosto',
+      '2 colheres de sopa de farinha de trigo',
+      '1/2 xícara (chá) de óleo',
+      '1 xícara de molho barbecue'
+    ],
+    preparation: [
+      'Em uma tigela coloque o encontro de asinha de galinha e polvilhe a farinha de trigo e misture com as mãos.',
+      'Em uma frigideira ou assador coloque o óleo quando estiver quente frite até ficarem douradas.',
+      'Para servir fica bonito com salada, ou abuse da criatividade.'
+    ],
+    information: ''
+  },
+  {
+    id_recipe: 'carbonara.jpeg',
+    image: '/assets/carbonara.jpeg',
+    title: 'Espaguete à Carbonara',
+    author: 'por Waylen Kirin',
+    ingredients: [
+      '600g de espaguete',
+      '3 ovos inteiros',
+      '2 gemas de ovos',
+      '250g de Almofada',
+      'Pecorino Romano ralado, a gosto',
+      'Sal, a gosto'
+    ],
+    preparation: [
+      'Encha uma panela com água em abundância, deixe ferver e depois salgue-a.',
+      'Corte o bacon em tiras. E já aqui alguém poderia argumentar que é melhor cortá-lo em cubos. Faça como quiser, a vantagem de cortá-la em tiras é que ela ficará mais crocante e mais consistente. Corte em cubos, apenas a gordura solta e alguns fragmentos permanecem.',
+      'Brown em uma panela, depois de polvilhar com pimenta. Não é necessário óleo, o bacon já é gordo, oleoso e fabuloso por si só.',
+      'Bata os ovos em uma tigela, tempere com pimenta e 3/4 de Pecorino e misture bem.',
+      'Cozinhe o macarrão al dente, escorra e refogue em uma panela quente para temperar adequadamente com a gordura do bacon. Para obter um resultado ideal, escorra o espaguete al dente, jogue-o em uma panela, mexa, adicione um pouco de água e termine de cozinhar.',
+      'Nesse ponto, chegamos ao momento tópico da massa carbonara, para não causar tensão, mas esse é o momento fugaz em que você pode fazer um prato imortal ou que o exporá à alegria do público. Você terá que ser rápido, implacável. Então prepare tudo com antecedência. Desligue o fogo e submerja com os ovos batidos e mexidos, continuando a adicionar o restante do pecorino.Tenha atenção à consistência, que deve ser cremosa, mas não fluida: fácil de dizer, mas não fazer. Se a massa secar demais, acalme-se com o pecorino! Não adicione água, não há pedaços, se as proporções não voltarem, o prato se foi ... depois de temperar o espaguete com ovos, não há como voltar atrás, da próxima vez será melhor. Se você quiser adicionar um pouco de água para cozinhar, faça-o antes de adicionar os ovos!'
+    ],
+    information: 'Sirva, polvilhe com pimenta moída na hora e desfrute de um dos ótimos pratos que fizeram a história da culinária italiana.'
+  },
+  {
+    id_recipe: 'panquecas.jpeg',
+    image: '/assets/panquecas.jpeg',
+    title: 'Panquecas',
+    author: 'por Rivaan Ludwin',
+    ingredients: [
+      '155g de farinha de trigo',
+      '40g de açucar refinado',
+      '1 colher (de chá) de fermento em pó',
+      '1 ovo',
+      '190ml de leite',
+      'Sal, a gosto',
+      '1 noz de manteiga'
+    ],
+    preparation: [
+      'Dentro de uma tigela, insira todos os componentes de farinha. Vamos começar inserindo farinha, fermento, açúcar e sal. Misture para misturar tudo uniformemente.',
+      'Agora despeje o ovo e o leite em um bule. Mexa até a mistura ficar homogênea.',
+      'Agora, inserimos o que foi obtido no ponto (1) ao composto líquido recém-preparado (2), misturando até obter um resultado semelhante ao obtido na imagem. Após esta fase, colocamos a mistura na geladeira para descansar por 1 hora.',
+      'Retire a mistura e misture com uma colher.',
+      'Vamos para a fase de cozimento. Unte a panela com uma noz de manteiga. Despeje a quantidade desejada de produto diretamente da embalagem ou com a ajuda de uma concha na panela.',
+      'Mantenha fogo até que o topo comece a mostrar bolhas. Neste ponto, você pode ligá-lo. Deixe o outro lado no fogão pela mesma quantidade de tempo. Vire mais algumas vezes. Et Voila está pronto.',
+    ],
+    information: ''
+  },
+  {
+    id_recipe: 'frita.jpeg',
+    image: '/assets/frita.jpeg',
+    title: 'Frita com Presunto e Mussarela',
+    author: 'por Lynne Sanders',
+    ingredients: [
+      '4 ovos',
+      'Salsa picada',
+      '45g de Parmesão',
+      'Sal e pimenta, a gosto',
+      'Azeite extra virgem',
+      '3 fatias de presunto cozido',
+      '10g de Mozzarellla'
+    ],
+    preparation: [
+      'Comece cortando a salsa e reserve em uma tigela.',
+      'Vamos começar a preparar a mussarela. Antes de trabalhar, lembre-se de deixá-lo em temperatura ambiente por 30 minutos para que ele perca excesso de líquidos. Corte a mussarela primeiro em fatias e depois em tiras.',
+      'Agora vamos colocar os ovos em uma taça. Nós adicionamos um pouco de pimenta a gosto. Um pouco de sal. Uma garoa de azeite extra-virgem, parmesão e salsa.',
+      'Depois de terminar de preparar a base, mexa bem para misturar os ingredientes.',
+      'Prepare a panela adicionando um pouco de óleo e unte toda a superfície da panela com um guardanapo. Coloque no fogo e assim que o óleo atingir a temperatura despeje metade da mistura. O tempo de cozimento é de cerca de 2 minutos de cada lado e gire-o frequentemente para evitar muita cor de um lado.',
+      'Depois que a primeira parte da mistura estiver cozida, despeje a panela na segunda metade. Depois de cozido, adicione a mussarela e o presunto cozido e cubra a outra omelete.',
+      'Neste ponto, tudo o que resta é deixar cozinhar por mais 2 minutos. Depois disso, desligue o fogo e deixe esfriar por cerca de 4 minutos antes de servir.',
+    ],
+    information: ''
+  },
+  {
+    id_recipe: 'sorvete.jpeg',
+    image: '/assets/sorvete.jpeg',
+    title: 'Gelato al Cioccolato',
+    author: 'por Lidiane Correa',
+    ingredients: [
+      '180g de chocolate amargo (70%)',
+      '1 colher de essência de baunilha',
+      '2 gemas',
+      '250ml de creme fresco',
+      '300ml de leite',
+      '100g de açucar',
+      '50ml de água',
+      '1 colher de farinha de sementes de alfarroba'
+    ],
+    preparation: [
+      'Começamos a preparação preparando a calda. Tomamos 50 gramas de açúcar e 50 ml de água e despejamos tudo em uma panela levando a ebolizzione atingindo a temperatura de 105 graus. Use um termômetro de cozinha para remover a calda do fogo somente depois de atingir a temperatura desejada.',
+      'Numa boule grande, coloque as 2 gemas, 50 gramas de açúcar, a essência da baunilha e a farinha de semente de carrubba. Continuamos a misturar tudo até obter uma mistura espumosa.',
+      'Colocamos uma panela no fogo e despejamos o leite, o creme e o chocolate preto em cubos. Mexa até o chocolate derreter e despeje o xarope preparado na hora. Mantenha a mistura no fogo até a temperatura de 80 graus.',
+      'Coloque a panela na superfície de trabalho e despeje a mistura preparada no ponto 2. Misture bem até obter um resultado homogêneo. O resultado será nossa base para sorvete de chocolate.',
+      'Deixe a mistura coberta com filme plástico esfriar até a temperatura ambiente, coloque-a no freezer e deixe descansar por pelo menos 2 horas.',
+      'Após 2 horas, saia do freezer e misture. Você notará que a mistura é muito cremosa e não é um único bloco congelado. Coloque a mistura de volta no freezer por mais 40 minutos e repita a passagem, misturando tudo até atingir a consistência típica do sorvete.'
+    ],
+    information: ''
+  },
+  {
+    id_recipe: 'medalhao.jpg',
+    image: '/assets/medalhao.jpg',
+    title: 'Medalhão de Carne Moida',
+    author: 'por Clarice Falcão',
+    ingredients: [
+      '600g de patinho moído duas vezes',
+      '1 cebola média ralada',
+      '3 colheres (sopa) de salsinha picada',
+      '2 dentes de alho triturados',
+      'Sal e pimenta-do-reino',
+      '120g de bacon em fatias finas para envolver',
+      '2 colheres (sopa) de azeite de oliva',
+      '1 dente de alho triturado',
+      '1/2 cebola pequena picadinha',
+      '1 colher (sobremesa) de amido de milho',
+      '1 e 1/2 xícara (chá) de água',
+      '1/2 xícara (chá) de molho de tomate',
+      '1/4 xícara (chá) de molho inglês',
+      '1/4 xícara (chá) de champignon laminado'
+    ],
+    preparation: [
+      'Sove bem a carne para dar liga. Junte a cebola, a salsinha, o alho e sal e pimenta a gosto e misture bem. Deixe na geladeira por 1 hora.',
+      'Faça bolas de aproximadamente 100 g cada e achate-as em formato de hambúrguer com cerca 2 cm de altura. Envolva a lateral de cada hambúrguer com o bacon fatiado. (Não é necessário prender com palitos.)',
+      'Forre uma assadeira com papel-alumínio e unte com óleo. Distribua os medalhões e leve ao forno médio-alto preaquecido (220 °C) por 15 minutos ou até atingir o ponto desejado. Se preferir, vire os medalhões após 10 minutos.',
+      'Aqueça o azeite e doure levemente o alho e a cebola. Junte o amido diluído na água, o molho de tomate, o molho inglês e sal e pimenta a gosto e cozinhe, mexendo sempre, até engrossar. Deixe ferver por mais 3 minutos e agregue o champignon.'
+    ],
+    information: 'Para servir, disponha o molho em uma travessa e arrume os medalhões por cima.'
+  },
+  {
+    id_recipe: 'pao-de-queijo.jpeg',
+    image: '/assets/pao-de-queijo.jpeg',
+    title: 'Pão de Queijo',
+    author: 'por Walace Freitas',
+    ingredients: [
+      '1 xícara de leite',
+      '1/2 xícara de óleo',
+      '1/2 colher (sopa) rasa de sal',
+      '500g de polvilho doce',
+      '2 ovos',
+      '1 1/2 xícara de queijo meia cura ralado'
+    ],
+    preparation: [
+      'Coloque o leite, o óleo e o sal numa leiteira. Leve ao fogo alto e, quando levantar fervura, despeje a mistura sobre o polvilho previamente colocado numa vasilha.',
+      'Mexa bem com uma colher de pau, para que o polvilho fique impregnado de líquido. Em seguida, acrescente os ovos, um a um, sempre mexendo com a colher de pau.',
+      'Junte o queijo ralado e misture com as mãos até que a massa não grude nas mãos. Sove a massa sobre uma superfície enfarinhada, até que ela fique homogênea e macia.',
+      'Em seguida, enrole pequenas bolinhas na palma da mão, dispondo-as numa assadeira retangular untada e enfarinhada. Leve para assar em forno preaquecido a 200°C por 20 minutos.'
+    ],
+    information: ''
+  }
+];
